20714a66126341c634e4ea48cfab2da14d30742e
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-11-21 19:46:29 +0000

    Remove `-fsycl` getting added to non-SYCL code

--- cmake/FindDPCPP.cmake
+++ cmake/FindDPCPP.cmake
@@ -7,9 +7,9 @@ message(CHECK_START "Looking for DPC++")
 unset(missingDpcppComponents)
 
 find_path(
-  SYCL_INCLUDE_DIRS
+  SYCL_INCLUDE_DIR
   NAMES
-    sycl.hpp
+    sycl/sycl.hpp
   PATHS
     ENV SYCL_ROOT
     /opt/intel/oneapi/compiler/latest/linux
@@ -17,8 +17,7 @@ find_path(
   PATH_SUFFIXES
     include/sycl
     include/CL
-    include/sycl/CL
-)
+    include/sycl/CL)
 
 find_library(
   SYCL_LIBRARIES
@@ -29,34 +28,21 @@ find_library(
     /opt/intel/oneapi/compiler/latest/linux
     ${SYCL_ROOT}
   PATH_SUFFIXES
-    lib
-)
-
-if(NOT DEFINED SYCL_FLAGS)
-  if(DEFINED ENV{SYCL_FLAGS})
-    set(SYCL_FLAGS $ENV{SYCL_FLAGS})
-  else()
-    set(SYCL_FLAGS -fsycl)
-  endif()
-endif()
+    lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     DPCPP
     REQUIRED_VARS
-    SYCL_INCLUDE_DIRS
-    SYCL_LIBRARIES
-    SYCL_FLAGS
-    )
+    SYCL_INCLUDE_DIR
+    SYCL_LIBRARIES)
 
 if(DPCPP_FOUND AND NOT TARGET OCCA::depends::DPCPP)
   # Create our wrapper imported target
   # Put it in the OCCA namespace to make it clear that we created it.
   add_library(OCCA::depends::DPCPP INTERFACE IMPORTED)
-  separate_arguments(SYCL_FLAGS UNIX_COMMAND "${SYCL_FLAGS}")
-  target_compile_options(OCCA::depends::DPCPP INTERFACE ${SYCL_FLAGS})
   set_target_properties(OCCA::depends::DPCPP PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${SYCL_INCLUDE_DIRS}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SYCL_INCLUDE_DIR};${SYCL_INCLUDE_DIR}/sycl"
     INTERFACE_LINK_LIBRARIES "${SYCL_LIBRARIES}"
   )
 endif()
